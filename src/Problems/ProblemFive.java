d42279002d0a279a308b554cb5b350d40fa92682
mr-jojo8 <mr.jojo8@gmail.com>
2013-01-28 16:46:42 +0800

    New File-

---
+++ src/Problems/ProblemFive.java
@@ -0,0 +1,16 @@
+package Problems;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class ProblemFive {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
