f2de10a8fa46bcf4ddcf502cad7ce718c456e958
mr-jojo8 <mr.jojo8@gmail.com>
2013-01-28 16:48:38 +0800

    Update src/Problems/ProblemFive.java
    in gitHub

--- src/Problems/ProblemFive.java
+++ src/Problems/ProblemFive.java
@@ -12,5 +12,6 @@ package Problems;
 public class ProblemFive {
     public static void main(String args[]){
         System.out.println("Hello World");
+        System.out.println("Nice GitHub");
     }
 }
